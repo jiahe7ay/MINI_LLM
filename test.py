df0e9d5af3fa4b66472871c290538627eca4ab29
Lil2J <33346376+jiahe7ay@users.noreply.github.com>
2024-03-01 16:46:10 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,40 @@
+from transformers import GenerationConfig
+import torch
+from qwen.tokenization_qwen import QWenTokenizer
+from qwen.modeling_qwen import QWenLMHeadModel
+
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model=QWenLMHeadModel.from_pretrained('./model_save/sft/checkpoint-500').to(device)
+
+tokenizer=QWenTokenizer.from_pretrained('./model_save/sft/checkpoint-500')
+
+gen_config=GenerationConfig(
+	temperature=0.7,
+	top_k=50,
+	top_p=0.95,
+	do_sample=True,
+	num_beams=1,
+	repetition_penalty=1.1,
+	max_new_tokens=300,
+	eos_token_id=tokenizer.eos_token_id,
+	pad_token_id=tokenizer.pad_token_id,
+
+	)
+
+print("程序启动")
+
+while True:
+	print("我：",end="")
+	text=input()
+	text="你是一个助手 用户: {text} 回答: ".format(text=text)
+	tokend=tokenizer(text,add_special=False)
+	input_ids,attention_mask=torch.LongTensor([tokend.input_ids]).to(device),\
+		torch.LongTensor([tokend.attention_mask]).to(device)
+	outputs=model.generate(
+		inputs=input_ids,
+		attention_mask=attention_mask,
+		generation_config=gen_config
+		)
+	outs=tokenizer.decode(outputs[0].cpu().numpy())
+	print("AI:"+outs.split("<|im_end|>")[0].split("回答: ")[1])
---
+++ train.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+accelerate launch --multi_gpu  --config_file accelerate_multi_gpu.yaml $1
+# accelerate launch $1
+
+# 使用方式，后面接一个训练脚本即可， 如：./train.sh sft.py
